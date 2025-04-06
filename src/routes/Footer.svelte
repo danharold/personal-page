9eff08645a79f1acfa0b67797698c21e2b588c0a
danharold <dan.harold2001@hotmail.com>
2025-04-06 22:57:42 +0100

    update header and footer, add console typer to header with fun messages

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -4,32 +4,6 @@
 	import { GithubIcon, Mail, MapPin } from 'lucide-svelte';
 </script>
 
-<!-- <footer>
-	<Separator />
-	<div class="bg-background flex w-full max-w-screen-lg content-center items-center px-2 py-5">
-		<p>danharold.com &copy {new Date().getFullYear()}</p>
-	</div>
-</footer> -->
-
-<!-- <footer id="contact" class="text-center">
-	<Separator />
-	<div class="py-4 text-center">
-		<p class="text-muted-foreground mb-4">Get in touch:</p>
-		<div class="flex justify-center space-x-4">
-			<Button variant="outline" size="icon">
-				<a href="/" target="_blank" aria-label="GitHub"><Github strokeWidth={1} /> </a>
-			</Button>
-			<Button variant="outline" size="icon">
-				<a href="/" target="_blank" aria-label="LinkedIn"><Linkedin strokeWidth={1} /></a>
-			</Button>
-			<Button variant="outline" size="icon">
-				<a href="mailto:dan@danharold.com" aria-label="Email"><Mail strokeWidth={1} /></a>
-			</Button>
-		</div>
-		<p class="text-muted-foreground mt-6 text-sm">&copy; {new Date().getFullYear()} Dan Harold</p>
-	</div>
-</footer> -->
-
 <footer id="contact">
 	<Separator />
 	<div class="py-4">
@@ -46,7 +20,7 @@
 			</Button>
 		</div>
 		<p class="text-muted-foreground mt-4 text-center text-sm">
-			Dan Harold &copy; {new Date().getFullYear()}
+			danharold.com &copy; {new Date().getFullYear()}
 		</p>
 	</div>
 </footer>
